eb4ede6b3e9ca74fdd4f66331b6c31daf9d6b087
Avinash Verma 3 <aver47@sapient.com>
2017-10-03 23:11:51 +0530

    add blockswaprotation file

---
+++ src/com/gfg/arr/problems/september/BlockSwapRotation.java
@@ -0,0 +1,17 @@
+package com.gfg.arr.problems.september;
+
+/*
+ * Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+ */
+
+public class BlockSwapRotation {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void name() {
+		
+	}
+	
+}
